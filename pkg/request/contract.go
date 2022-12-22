12201a189932bb862b324ac6a866162d037f6e60
trkhoi <ngotrongkhoi112@gmail.com>
2022-12-22 14:14:03 +0700

    chore: add collection with priority

--- pkg/request/contract.go
+++ pkg/request/contract.go
@@ -1,8 +1,9 @@
 package request
 
 type AddContractRequest struct {
-	Address string `json:"address"`
-	ChainId int64  `json:"chain_id" binding:"omitempty,gte=0" msg:"invalid chain id"`
-	Name    string `json:"name"`
-	Symbol  string `json:"symbol"`
+	Address      string `json:"address"`
+	ChainId      int64  `json:"chain_id" binding:"omitempty,gte=0" msg:"invalid chain id"`
+	Name         string `json:"name"`
+	Symbol       string `json:"symbol"`
+	PriorityFlag bool   `json:"priority_flag"`
 }
