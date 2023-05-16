401695c2036ff533418393df6f6b047000460650
Huy Nguyen Quang <39359294+quanghuynguyen1902@users.noreply.github.com>
2023-05-16 11:05:30 +0700

    chore: add sui mainnet config

--- pkg/queue/queue.go
+++ pkg/queue/queue.go
@@ -180,6 +180,8 @@ func (q *Queue) ConsumerGroup(chainId int64) string {
 		return q.cfg.Kafka.RoninConsumerGroup
 	case 9997:
 		return q.cfg.Kafka.SuiConsumerGroup
+	case 9996:
+		return q.cfg.Kafka.AptosConsumerGroup
 	case 1975:
 		return q.cfg.Kafka.OnusConsumerGroup
 	default:
